312415670277a0f6c8b137d39a6b03c3755ed678
Duncan Freeman <dfreeman@lucidyne.com>
2022-01-05 15:17:35 -0800

    use dummy import to force the linker

--- add_one_op/add_one.cpp
+++ add_one_op/add_one.cpp
@@ -1,5 +1,12 @@
 #include <torch/extension.h>
 
+/// In order to load this dynamic library up with C++ or Rust, we need to fool the linker into thinking we use the library from our code.
+#ifdef _WIN32 
+extern "C" void __declspec( dllexport ) test_library() { std::cout << "Custom op loaded on Windows" << std::endl; }
+#else
+extern "C" void test_library() { std::cout << "Custom op loaded on Linux" << std::endl; }
+#endif
+
 torch::Tensor add_one(torch::Tensor input) {
     return input + torch::ones_like(input);
 }
--- test_op.cpp
+++ test_op.cpp
@@ -1,6 +1,11 @@
 #include <torch/script.h>
 
+extern "C" {
+    void test_library();
+}
+
 int main(int argc, const char* argv[]) {
+    test_library();
     torch::jit::Module module = torch::jit::load("traced.pt");
     std::cout << "OK" << std::endl;
 }
